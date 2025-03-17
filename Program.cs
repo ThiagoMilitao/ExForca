d0f84de68b2d9b8e4fe7f8f0d2074ecb45206108
Thiago Militão <thiago.militao2007@gmail.com>
2025-03-17 10:04:54 -0300

    forca

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿String letra, palavra;
+
+Console.WriteLine("Digite a palavra do jogo");
+palavra = Console.ReadLine();
+
+Console.WriteLine("Digite uma letra: ");
+letra = Console.ReadLine();
+
+
+if(palavra.IndexOf(letra) >= 0){
+    Console.Write("Acertou! A letra digitada existe na palavra.");
+}else{
+    Console.Write("Errou! A letra digitada não existe na palavra.");
+}
+
